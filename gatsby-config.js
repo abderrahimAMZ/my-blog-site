4fe841dbed68f06b180b47d762c1acda80de223c
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2023-11-02 23:27:44 +0100

    second commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my blog site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
