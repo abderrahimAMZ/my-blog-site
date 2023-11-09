abed9d1e3ce58dc7574871d0f3ed506a9e0f4c6a
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2023-11-09 12:02:11 +0100

    Finished Gatsby tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
              <h1>Welcome to my Gatsby site!</h1>
              <p>I'm making this by following the Gatsby Tutorial.</p>
              <StaticImage src={"https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"} alt={"Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"} />
-
+             <StaticImage src="../images/bootloader.png" alt={"a bootloader loaded in memory image"} />
          </Layout>
       </main>
   )
