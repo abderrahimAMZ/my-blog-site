4f830b61f418787f96fcaf003af855cf1975e31b
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2023-11-23 13:21:03 +0100

    fixing some stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from "../components/layout";
-import {Link} from "gatsby";
-
+import Seo from "../components/seo"
 // Step 2: Define your component
 const AboutPage = () => {
     return (
@@ -12,7 +11,8 @@ const AboutPage = () => {
             </Layout>
         </main>
     )
-}
+};
+export const Head = () => <Seo title={"About me"} />
+export default AboutPage;
 
 // Step 3: Export your component
-export default AboutPage
