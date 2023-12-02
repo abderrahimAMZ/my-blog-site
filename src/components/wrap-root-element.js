9a6477458fbf0f882adfe923ba4825f341139aa7
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2023-12-02 19:02:01 +0100

    finished code blocks part

--- src/components/wrap-root-element.js
+++ src/components/wrap-root-element.js
@@ -1,39 +1,9 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
-import {Highlight,themes, defaultProps } from "prism-react-renderer";
-
-// Other styling imports
+import Code from "./code";
 
 const components = {
-    pre: props => {
-        console.log(props);
-        const className = props.children.props.className || "";
-        const code = props.children.props.children.trim();
-        const language = className.replace(/language-/, "");
-        const file = props.children.props.file;
-        return (
-            <Highlight
-                {...defaultProps}
-                code={code}
-                language="rust"
-                theme={themes.vsDark}
-            >
-                {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                    <pre className={className} style={style}>
-            <div>{`Language: ${language}`}</div>
-                        {<div>{file && `File: ${file}`}</div>}
-                        {tokens.map((line, i) => (
-                            <div {...getLineProps({ line, key: i })}>
-                                {line.map((token, key) => (
-                                    <span {...getTokenProps({ token, key })} />
-                                ))}
-                            </div>
-                        ))}
-          </pre>
-                )}
-            </Highlight>
-        );
-    },
+    pre: props => <Code {...props} />,
     wrapper: ({ children }) => <>{children}</>,
 };
 
